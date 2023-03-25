1279b81f5826692071b61a02cd08aa7a0e9bfb37
ImGabreuw <imgabreuw.dev@gmail.com>
2023-03-24 21:03:23 -0300

    Aula 23: Introdução às variáveis em Python

---
+++ secao-3-iniciando-na-programcao-com-python/aula-23/projetos/main.py
@@ -0,0 +1,15 @@
+nomem_completo = "Luiz Otávio Miranda"
+soma_2_mais_2 = 2 + 2
+print(nomem_completo, soma_2_mais_2)
+
+int_1 = int('1')
+print(int_1, type(int_1))
+
+true = bool('1')
+print(true)
+
+nome = "Luiz"
+idade = 30
+maior_de_idade = idade >= 18
+print("Nome:", nome, "Idade:", idade)
+print("É maior?", maior_de_idade)
