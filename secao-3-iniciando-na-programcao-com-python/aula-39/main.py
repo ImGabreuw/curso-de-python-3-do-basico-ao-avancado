3a783960effc9341d3d2ae5645d6092514303592
ImGabreuw <imgabreuw.dev@gmail.com>
2023-06-05 17:41:43 -0300

    Aula 39: Exercício de programação com if e comparação

---
+++ secao-3-iniciando-na-programcao-com-python/aula-39/main.py
@@ -0,0 +1,7 @@
+primeiro_valor = input()
+segundo_valor = input()
+
+if primeiro_valor > segundo_valor:
+    print(f"{primeiro_valor=} é maior do que {segundo_valor=}")
+else:
+    print(f"{segundo_valor=} é maior do que {primeiro_valor=}")
